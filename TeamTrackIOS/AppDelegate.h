bbcba3e285a6c54cde0e9d3f84e2137b16f266af
stanleegoodspeed <ccole3390@gmail.com>
2015-08-03 17:55:52 -0400

    Initial Commit

---
+++ TeamTrackIOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TeamTrackIOS
+//
+//  Created by Colin Cole on 8/3/15.
+//  Copyright (c) 2015 Colin Cole. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TeamTrackIOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TeamTrackIOS
+//
+//  Created by Colin Cole on 8/3/15.
+//  Copyright (c) 2015 Colin Cole. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
